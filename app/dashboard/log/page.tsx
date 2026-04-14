1092f50a1392b6f09d684360b1b0b79c5878c62c
filip patrman <filip.patrman.023@pslib.cz>
2026-04-14 14:15:30 +0200

    refactor: type cast user_skills data to UserSkills interface in LogPage

--- app/dashboard/log/page.tsx
+++ app/dashboard/log/page.tsx
@@ -1,20 +1,29 @@
 import { createClient } from "@/utils/supabase/server";
 import LogClient from "./LogClient";
+import { UserSkills } from "@/types/training";
 
 export default async function LogPage() {
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  if (!user) return
+  if (!user) return;
   const [skillsResponse, userSkillsResponse] = await Promise.all([
     supabase.from("skills").select("*"),
-    supabase.from("user_skills").select("*").eq("user_id", user.id)
-  ])
-  
+    supabase.from("user_skills").select("*").eq("user_id", user.id),
+  ]);
+
   if (skillsResponse.error || userSkillsResponse.error) {
-    console.error("Failed to load skills dictionary:", skillsResponse.error?.message);
+    console.error(
+      "Failed to load skills dictionary:",
+      skillsResponse.error?.message,
+    );
     return;
   }
-  return <LogClient dictionary={skillsResponse.data || []} userSkills={userSkillsResponse.data || []} />;
+  return (
+    <LogClient
+      dictionary={skillsResponse.data || []}
+      userSkills={(userSkillsResponse.data as UserSkills[]) || []}
+    />
+  );
 }
