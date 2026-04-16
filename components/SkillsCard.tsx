4bb5899c557d2c5f0b444742fa9c4ca3a12ba4cd
filip patrman <filip.patrman.023@pslib.cz>
2026-04-16 14:27:50 +0200

    feat: add loading state and transition handling to skill status updates in SkillsCard

--- components/SkillsCard.tsx
+++ components/SkillsCard.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { updateSkillStatus } from "@/services/skills.service";
 import { SkillLibrary } from "@/types/training";
-import { BookOpen, CircleCheckBig, CircleDashed } from "lucide-react";
+import { BookOpen, CircleCheckBig, CircleDashed, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 export default function SkillsCard({ skill }: { skill: SkillLibrary }) {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
   const statusLabels = {
     not_started: "Not Started",
     learning: "Learning",
@@ -25,13 +27,19 @@ export default function SkillsCard({ skill }: { skill: SkillLibrary }) {
   const handleStatusChange = async (
     newStatus: "not_started" | "learning" | "mastered",
   ) => {
-    await updateSkillStatus(skill.id, newStatus);
-    router.refresh();
+    startTransition(async () => {
+      await updateSkillStatus(skill.id, newStatus);
+      router.refresh();
+    });
   };
   return (
     <div className="flex justify-between shadow-xs rounded-xl p-4 bg-card border border-border items-center">
       <div className="flex items-center gap-2">
-        {statusIcons[skill.status]}
+        {isPending ? (
+          <Loader2 className="w-10 h-10 animate-spin text-muted-foreground bg-muted p-2 rounded-full" />
+        ) : (
+          statusIcons[skill.status]
+        )}
         <div className="flex flex-col">
           <h2 className="text-foreground font-bold">{skill.name}</h2>
           <div className="flex flex-wrap gap-x-3 gap-y-1">
@@ -50,11 +58,12 @@ export default function SkillsCard({ skill }: { skill: SkillLibrary }) {
       <div className="flex flex-col items-center gap-2 shrink-0">
         <div className="relative flex">
           <div
-            className={`ml-2 hover:cursor-pointer hover:scale-105 transition-transform text-sm text-white rounded-full px-3 py-1 ${skill.status === "mastered" ? "bg-primary" : skill.status === "learning" ? "bg-secondary" : "bg-muted-foreground"}`}
+            className={`ml-2 hover:cursor-pointer hover:scale-105 transition-transform text-sm text-white rounded-full px-3 py-1 ${skill.status === "mastered" ? "bg-primary" : skill.status === "learning" ? "bg-secondary" : "bg-muted-foreground"} ${isPending ? "opacity-50 cursor-not-allowed" : "hover:scale-105"}`}
           >
             {statusLabels[skill.status]}
           </div>
           <select
+          disabled={isPending}
             value={skill.status}
             onChange={(e) =>
               handleStatusChange(e.target.value as SkillLibrary["status"])
